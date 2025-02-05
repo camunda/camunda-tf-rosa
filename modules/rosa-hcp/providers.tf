2959f75833eb380e11ad81828f8bdf0c1c8dfb7f
Leo J. <153937047+leiicamundi@users.noreply.github.com>
2025-02-05 22:21:22 +0100

    add providers required

--- modules/rosa-hcp/providers.tf
+++ modules/rosa-hcp/providers.tf
@@ -1,1 +1,14 @@
 data "aws_caller_identity" "current" {}
+
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+    rhcs = {
+      source = "terraform-redhat/rhcs"
+    }
+  }
+}
